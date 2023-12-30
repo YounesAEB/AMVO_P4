2a3da9b5c7173f01176cc0953d8bc7f0e8d06790
joelrajo <100616947+joelrajo@users.noreply.github.com>
2023-12-30 19:26:38 +0100

    Changes

--- computeNumSatsOnSight.m
+++ computeNumSatsOnSight.m
@@ -1,11 +1,12 @@
-function Z = computeNumSatsOnSight(coordECEF,globalGrid)
+function Z = computeNumSatsOnSight(coordECEF,spherCoordECEF,globalGrid,globalGrid_polar)
 satellitesInView  = zeros(size(coordECEF,1),4); % Matrix for the whole planet
-%v = VideoWriter("onSightSat1.avi");
-%open(v)
+% v = VideoWriter("onSight.avi");
+% open(v)
+
 for t=1:size(coordECEF,1)
-number_grid = zeros(180/5,360/5);
-for i=1:180/5
-    for j=1:360/5
+number_grid = zeros(180+1,360+1);
+for i=1:180+1
+    for j=1:360+1
         X0 = globalGrid(i,j,1);
         Y0 = globalGrid(i,j,2);
         Z0 = globalGrid(i,j,3);
@@ -15,23 +16,22 @@ for i=1:180/5
 end
     
 % Video generator
-%X = globalGrid_polar(:,:,3);
-%Y = globalGrid_polar(:,:,2);
 Z = number_grid;
-
+% X = globalGrid_polar(:,:,3);
+% Y = globalGrid_polar(:,:,2);
 % figure;
-% surf(globalGrid(:,:,1),globalGrid(:,:,2),globalGrid(:,:,3),Z)
-% hold on
-% scatter3(coordECEF(t,1),coordECEF(t,2),coordECEF(t,3),'filled','o','MarkerFaceColor','r');
-% axis equal
+% % surf(globalGrid(:,:,1),globalGrid(:,:,2),globalGrid(:,:,3),Z)
+% % hold on
+% % scatter3(coordECEF(t,1),coordECEF(t,2),coordECEF(t,3),'filled','o','MarkerFaceColor','r');
+% % axis equal
 % hold on
 % surf(X, Y, Z,'EdgeColor', 'None', 'facecolor', 'interp');
 % colormap jet;
 % colorbar
-% scatter3(deg2rad(polar1(t,3)),deg2rad(polar1(t,2)),5,'filled','o','MarkerFaceColor','r');
-% % scatter3(deg2rad(polar2(t,3)),deg2rad(polar2(t,2)),5,'filled','o','MarkerFaceColor','b');
-% % scatter3(deg2rad(polar3(t,3)),deg2rad(polar3(t,2)),5,'filled','o','MarkerFaceColor','g');
-% % scatter3(deg2rad(polar4(t,3)),deg2rad(polar4(t,2)),5,'filled','o','MarkerFaceColor','y');
+% scatter3(deg2rad(spherCoordECEF(t,3,1)),deg2rad(spherCoordECEF(t,2,1)),5,'filled','o','MarkerFaceColor','k');
+% scatter3(deg2rad(spherCoordECEF(t,3,2)),deg2rad(spherCoordECEF(t,2,2)),5,'filled','o','MarkerFaceColor','k');
+% scatter3(deg2rad(spherCoordECEF(t,3,3)),deg2rad(spherCoordECEF(t,2,3)),5,'filled','o','MarkerFaceColor','k');
+% scatter3(deg2rad(spherCoordECEF(t,3,4)),deg2rad(spherCoordECEF(t,2,4)),5,'filled','o','MarkerFaceColor','k');
 % view(2);
 % axis equal; 
 % axis off;
@@ -47,6 +47,6 @@ threeSatellite = sum(sum(number_grid==3));
 fourSatellite  = sum(sum(number_grid==4));
 satellitesInView(t,:) = [oneSatellite,twoSatellite,threeSatellite,fourSatellite];
 end   
-%close(v) 
+% close(v) 
 
 end
