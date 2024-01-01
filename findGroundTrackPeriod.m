3381931b1aa2807072ef91e120896ba03c8ae384
PolRuiz <pol.ruiz.celada@estudiantat.upc.edu>
2024-01-01 21:37:29 +0100

    Update findGroundTrackPeriod.m

--- findGroundTrackPeriod.m
+++ findGroundTrackPeriod.m
@@ -1,26 +1,48 @@
-function timeTaken = findGroundTrackPeriod(spherCoordECEF,time)
+function timeTaken = findGroundTrackPeriod(spherCoordECEF,time,nSat)
 
+relativeTime    = time(:) - time(1);
 orbitalPeriod   = 95400;
-relativeTime    = time(:)-time(1);
-longitude      = spherCoordECEF(:,3,2);
+firstOrbitIndex = find(relativeTime == orbitalPeriod);
+timeTaken       = zeros(nSat,1);
 
-i = find(relativeTime==orbitalPeriod); %ignore first orbit
-periodicity = false;
-
-
-while periodicity == false
-    if (longitude(i) < longitude(1)) && (longitude(i) > longitude(2))
-        periodicity = true;
-        timeTaken = relativeTime(i);
-    else
-        i = i+1;
+for j = 1 : nSat    
+    longitude     = spherCoordECEF(:,3,j);
+    firstOrbitLongitudes = (longitude(1:firstOrbitIndex));
+    relativeLongitudes = zeros(size(firstOrbitLongitudes,1),1);
+    for k = 1:size(firstOrbitLongitudes,1)
+        if (firstOrbitLongitudes(1) < 0) && (firstOrbitLongitudes(k) < 0)
+            relativeLongitudes(k) = firstOrbitLongitudes(k) + 360;
+        else
+            relativeLongitudes(k) = firstOrbitLongitudes(k);
+        end
     end
-    if i == size(time,1)+1
-        periodicity = true;
-        disp('The ground track does not repeat itself in the given time window.');
-        timeTaken = -1;
+    
+    [~,goalIndex] = max(relativeLongitudes);
+    goalLongitude = firstOrbitLongitudes(goalIndex);  
+    initialTime = relativeTime(goalIndex);
+    
+
+    elegibleIndexes    = find(longitude(firstOrbitIndex+1:end) > goalLongitude-5);
+    elegibleLongitude  = longitude(elegibleIndexes+firstOrbitIndex);
+    elegibleTime       = relativeTime(elegibleIndexes+firstOrbitIndex);    
+    i           = 1;
+    periodicity = false;
+
+    while periodicity == false
+        if (elegibleLongitude(i) < goalLongitude)
+            periodicity = true;
+            timeTaken(j) = elegibleTime(i)-initialTime;
+        else
+            i = i+1;
+        end
+        if i == size(elegibleLongitude,1)+1
+            periodicity = true;
+            % disp('The ground track does not repeat itself in the given time window.');
+            timeTaken(j) = -1;
+        end
     end
 end
+
 
 end
 
